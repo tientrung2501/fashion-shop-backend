87756d9696841d5ea8b994b7e4ecde8a7d9d5610
tientrung2501 <tientrung2501@gmail.com>
2022-09-04 11:24:24 +0700

    add inStock for product detail response

--- src/main/java/com/capstone/fashionshop/models/entities/product/ProductOption.java
+++ src/main/java/com/capstone/fashionshop/models/entities/product/ProductOption.java
@@ -3,6 +3,7 @@ package com.capstone.fashionshop.models.entities.product;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
@@ -26,9 +27,16 @@ public class ProductOption {
     @DocumentReference(lazy = true)
     @JsonIgnore
     private Product product;
+    @Transient
+    private Long inStock;
 
     public ProductOption(String name, BigDecimal extraFee) {
         this.name = name;
         this.extraFee = extraFee;
+    }
+
+    public Long getInStock() {
+        return variants.stream().map(ProductVariant::getStock)
+                .reduce(0L, Long::sum);
     }
 }
