d97d30c52928a03742f7191700f4fdd062115eeb
tientrung2501 <tientrung2501@gmail.com>
2022-10-19 19:26:08 +0700

    update failed in repository

--- src/main/java/com/capstone/fashionshop/repository/OrderRepository.java
+++ src/main/java/com/capstone/fashionshop/repository/OrderRepository.java
@@ -13,5 +13,5 @@ public interface OrderRepository extends MongoRepository<Order, String> {
     Optional<Order> findOrderByUser_IdAndState(ObjectId userId, String state);
     Page<Order> findAllByState(String state, Pageable pageable);
     Optional<Order> findOrderByPaymentDetail_PaymentTokenAndState(String token, String state);
-    Page<Order> findAllByCreatedDateBetweenAndState(LocalDateTime from, LocalDateTime to, String state);
+    Page<Order> findAllByCreatedDateBetweenAndState(LocalDateTime from, LocalDateTime to, String state, Pageable pageable);
 }
